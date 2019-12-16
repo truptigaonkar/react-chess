2b8aa2348008d40966c2bc7b04fbc1e0fa50e9ed
BASEL8 <basel84mn@gmail.com>
2019-12-16 14:43:47 +0100

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,31 @@
-module.exports={
- "extends" : "airbnb-base"
-}
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+  parser: 'babel-eslint',
+  env: {
+    jest: true,
+  } 
+};
