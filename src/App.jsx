e4c7e045cd4b60d80bd82d2605eba8fbd406af65
Elin Bjornetorp <bjornetorp@hotmail.com>
2019-12-19 14:17:45 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Chess from 'chess.js';
 import logo from './logo.svg';
 import './App.css';
-import Chess from 'chess.js';
 let chess = new Chess;
+let pgn = chess.pgn();
 
 function App() {
+
+  useEffect( () => {
+    console.log('chess: ', chess);
+    console.log('pgn: ', pgn);
+  });
+
   return (
     <div className="App">
-      chess
     </div>
   );
 }
