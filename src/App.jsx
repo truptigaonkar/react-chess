ca7bde0fcdd29c073c057c3c5caab0f4a943fe16
Elin Bjornetorp <bjornetorp@hotmail.com>
2019-12-23 11:07:58 +0100

    Game component in separate file

--- src/App.jsx
+++ src/App.jsx
@@ -2,80 +2,13 @@ import React, { useEffect, useState } from 'react';
 import Chess from 'chess.js';
 import Chessboard from 'chessboardjsx';
 import './App.css';
-let chess = new Chess();
-let pgn = chess.pgn();
+import
 
 function App() {
-  let [fen, updateFen] = useState('start');
-  let [moveHistory, updateMoveHistory] = useState([]);
-  let newFen = '';
 
-  function updateView() {
-    newFen = chess.fen();
-    updateFen(newFen);
-  }
-
-  function onDrop(sourceSquare) {
-    let sourceSq = sourceSquare.sourceSquare;
-    let targetSq = sourceSquare.targetSquare;
-    let piece = sourceSquare.piece;
-    let moveObject = {};
-    let newFen = '';
-
-    // console.log(sourceSq, targetSq, piece);
-
-    moveObject = chess.move({from: sourceSq, to: targetSq});
-
-    // console.log(moveObject);
-
-    if(moveObject) {
-      console.log('Legal move.');
-      updateView();
-      let updatedHistory = chess.history();
-      updateMoveHistory(updatedHistory);
-    }
-    else {
-      console.log('ILLEGAL MOVE');
-    }
-  }
-
-  function onClickReset(e) {
-    chess.reset();
-    updateView();
-  }
-
-  return (
-    <div className="App">
-      <Chessboard
-      position={fen}
-      onDrop={onDrop}
-      />
-      <MovesTable moveHistory={moveHistory}/>
-      <button onClick={onClickReset}>Restart</button>
-    </div>
-  );
+  return ();
 }
 
-function MovesTable(props) {
-  let moveHistory = props.moveHistory;
-  console.log(moveHistory);
 
-  let tableRows = moveHistory.map( move => {
-    return <tr><td>{move}</td></tr>;
-  });
-
-  return(
-    <table>
-      <thead>
-        <tr>
-          <th>Moves</th>
-        </tr>
-      </thead>
-      <tbody>
-        {tableRows}
-      </tbody>
-    </table>
-  );
-}
 
 export default App;
---
+++ src/Game.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from 'react';
+import Chess from 'chess.js';
+import Chessboard from 'chessboardjsx';
+import './App.css';
+let chess = new Chess();
+
+function Game() {
+  let [fen, updateFen] = useState('start');
+  let [moveHistory, updateMoveHistory] = useState([]);
+  let newFen = '';
+
+  function updateView() {
+    newFen = chess.fen();
+    updateFen(newFen);
+  }
+
+  function onDrop(sourceSquare) {
+    let sourceSq = sourceSquare.sourceSquare;
+    let targetSq = sourceSquare.targetSquare;
+    let piece = sourceSquare.piece;
+    let moveObject = {};
+    let newFen = '';
+
+    // console.log(sourceSq, targetSq, piece);
+
+    moveObject = chess.move({from: sourceSq, to: targetSq});
+
+    // console.log(moveObject);
+
+    if(moveObject) {
+      console.log('Legal move.');
+      updateView();
+      let updatedHistory = chess.history();
+      updateMoveHistory(updatedHistory);
+    }
+    else {
+      console.log('ILLEGAL MOVE');
+    }
+  }
+
+  function onClickReset(e) {
+    chess.reset();
+    updateView();
+  }
+
+  return (
+    <div className="App">
+      <Chessboard
+      position={fen}
+      onDrop={onDrop}
+      />
+      <MovesTable moveHistory={moveHistory}/>
+      <button onClick={onClickReset}>Restart</button>
+    </div>
+  );
+}
+
+function MovesTable(props) {
+  let moveHistory = props.moveHistory;
+  console.log(moveHistory);
+
+  let tableRows = moveHistory.map( move => {
+    return <tr><td>{move}</td></tr>;
+  });
+
+  return(
+    <table>
+      <thead>
+        <tr>
+          <th>Moves</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableRows}
+      </tbody>
+    </table>
+  );
+}
+
+export default Game;
