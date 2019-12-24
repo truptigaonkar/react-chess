140ab4a78961b0cb9486a4e58c3d31097133ce01
truptigaonkar <trups11.trupti@gmail.com>
2019-12-24 17:56:57 +0100

    Component Login: Checkbox Add Player added

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,9 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import { Grid, CssBaseline, Paper, Typography, Link, Avatar, FormControlLabel, Checkbox, Box, TextField, Button } from '@material-ui/core';
+import { Grid, CssBaseline, Paper, Typography, Avatar, FormControlLabel, Checkbox, Box, TextField, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import {ReactComponent as Crown} from '../resources/crown.svg';
 
@@ -25,7 +25,7 @@ const useStyles = makeStyles(theme => ({
     height: '100vh',
   },
   image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
+    //backgroundImage: 'url(https://source.unsplash.com/random)',
     backgroundRepeat: 'no-repeat',
     backgroundColor:
       theme.palette.type === 'dark' ? theme.palette.grey[900] : theme.palette.grey[50],
@@ -60,6 +60,7 @@ const Login = () => {
   const [username, setUsername] = useState([]); 
   const [errorMessage, setErrorMessage] = useState('');
   const [auth, setAuth] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
 
   // Handling username input
   const onChangeUsername = (e) => {
@@ -83,12 +84,58 @@ const Login = () => {
       });
   };
 
+  const toggleChange = () => {
+    setIsChecked(!isChecked);
+  }
+
   if (auth) {
     return (
       <Redirect to="/lobby" />
     );
   }
 
+  if (!isChecked) {
+    return (
+      <>
+      <Helmet><title>Chess online</title></Helmet>
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Crown className={classes.logo} />
+  {/*           <Avatar className={classes.avatar}>
+              <Crown />
+            </Avatar> */}
+            <Typography component="h1" variant="h5">
+              Chess
+            </Typography>
+            <FormControlLabel
+              control={<Checkbox color="primary" />}
+              label="Add player"
+              checked={isChecked}
+          onChange={toggleChange}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              data-testid="button"
+              href="/lobby"
+            >
+              Enter
+            </Button>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </div>
+        </Grid>
+      </Grid>
+      </>
+    );
+  }else{
   return (
     <>
     <Helmet><title>Chess online</title></Helmet>
@@ -104,6 +151,12 @@ const Login = () => {
           <Typography component="h1" variant="h5">
             Chess
           </Typography>
+       <FormControlLabel
+              control={<Checkbox color="primary" />}
+              label="Add player"
+              checked={isChecked}
+          onChange={toggleChange}
+            />
           <form className={classes.form} onSubmit={handleAddUser}>
             <TextField
               variant="outlined"
@@ -119,10 +172,6 @@ const Login = () => {
               autoFocus
             />
             <p style={{ color: 'red' }}>{errorMessage}</p>
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
             <Button
               type="submit"
               fullWidth
@@ -142,6 +191,6 @@ const Login = () => {
     </Grid>
     </>
   );
+        }
 }
-
 export default Login;
