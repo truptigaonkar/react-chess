d92b742d47b5115a1ee47357047ba7efc93e98dc
truptigaonkar <trups11.trupti@gmail.com>
2019-12-25 22:15:26 +0100

    Friend has been added successfully for each player. Console.log ok result

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 function Modal(props) {
   const { show, closeModal } = props;
+  const [friendname, setFriendname] = useState([]);
+
+  const handleFriendname = (e) => {
+    setFriendname(e.target.value);
+  }
+
+  const handleAddFriend = (e) =>{
+    e.preventDefault();
+    axios.post('http://localhost:8000/api/withFriend/', { userId: localStorage.getItem('userId'), friendId:friendname })
+      .then((res) => {    
+          console.log(res); 
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+  }
 
   return (
     <>
       <div className={show ? "modal" : "hide"}>
         <button className="button-closeModal" onClick={closeModal}>X</button>
         <h3>Play with friend</h3>
+        <form onSubmit={handleAddFriend}>
+        <input type="text" name="friendname" placeholder="friendname" onChange={handleFriendname} value={friendname}/>
+        <button type="submit">Add Friend</button>
+        </form>
       </div>
     </>
   );
