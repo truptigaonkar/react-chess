8793a7e12ecbd7a19046493cb853db42d9ed49e3
BASEL8 <basel84mn@gmail.com>
2019-12-27 12:19:08 +0100

    add remove game

--- backend/routes/game.js
+++ backend/routes/game.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const { matchData, gameMove, playGame } = require('../controller/game');
-const { gameMoveValidationValidation, playGameValidationValidation } = require('../validators/game');
+const {
+  matchData, gameMove, playGame, deleteGame,
+} = require('../controller/game');
+const { gameMoveValidationValidation, playGameValidationValidation, deleteGameValidation } = require('../validators/game');
 const { runValidation } = require('../validators');
 
 const router = express.Router();
@@ -9,5 +11,6 @@ const router = express.Router();
 router.get('/game/:_id', matchData);
 router.post('/game/move', gameMoveValidationValidation, runValidation, gameMove);
 router.post('/game/play', playGameValidationValidation, runValidation, playGame);
+router.post('/game/deleteUnActiveGame', deleteGameValidation, runValidation, deleteGame);
 
 module.exports = router;
