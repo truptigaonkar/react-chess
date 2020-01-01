9eef4fea874f2cb1d70da38416a436ea9cde09a6
Ayumi Nakamura <ayumi.nakamur@gmail.com>
2020-01-01 21:00:26 +0100

    Added tests for Get game

--- backend/tests/backend.test.js
+++ backend/tests/backend.test.js
@@ -3,6 +3,8 @@ const request = require('supertest');
 const url = 'http://localhost:8000/api';
 const testId = 'testUser';
 const shortId = 'boo';
+const validFriend = 'testFriend';
+const gameId = '5e04ad71e8296713c02b218f';
 
 describe('GET /seeks', () => {
   it('succeeds with /seeks/userId', async () => {
@@ -16,7 +18,7 @@ describe('GET /seeks', () => {
   it('fails without userId', async () => {
     const response = await request(url)
       .get('/seeks');
-      //.expect('Content-Type', /json/);
+      // .expect('Content-Type', /json/);
     expect(response.statusCode).toEqual(404);
   });
 });
@@ -55,7 +57,6 @@ describe('POST /seeks', () => {
 });
 
 describe('POST /withFriend', () => {
-  const validFriend = 'testFriend';
   it('succeeds when valid userId is sent', async () => {
     const response = await request(url)
       .post('/withFriend')
@@ -107,5 +108,56 @@ describe('POST /withFriend', () => {
     expect(typeof response.body).toBe('object');
     expect(response.body).toHaveProperty('err');
     expect(response.body.err).toEqual('friendId must be at least 5 characters long');
+  });
+});
+
+describe('GET /withFriendRequests', () => {
+  it('succeeds with /withFriendRequests/userId', async () => {
+    const response = await request(url)
+      .get(`/withFriendRequests/${testId}`)
+      .expect('Content-Type', /json/);
+    expect(response.statusCode).toEqual(200);
+    expect(typeof response.body).toBe('object');
+  });
+
+  it('fails without userId', async () => {
+    const response = await request(url)
+      .get('/withFriendRequests');
+      // .expect('Content-Type', /json/);
+    expect(response.statusCode).toEqual(404);
+  });
+});
+
+describe('GET /allUserGames', () => {
+  it('succeeds with /allUserGames/userId', async () => {
+    const response = await request(url)
+      .get(`/allUserGames/${testId}`)
+      .expect('Content-Type', /json/);
+    expect(response.statusCode).toEqual(200);
+    expect(typeof response.body).toBe('object');
+  });
+
+  it('fails without userId', async () => {
+    const response = await request(url)
+      .get('/allUserGames');
+      // .expect('Content-Type', /json/);
+    expect(response.statusCode).toEqual(404);
+  });
+});
+
+describe('GET /game', () => {
+  it('succeeds with /game/_id', async () => {
+    const response = await request(url)
+      .get(`/game/${gameId}`)
+      .expect('Content-Type', /json/);
+    expect(response.statusCode).toEqual(200);
+    expect(typeof response.body).toBe('object');
+  });
+
+  it('fails without _id', async () => {
+    const response = await request(url)
+      .get('/seeks');
+      // .expect('Content-Type', /json/);
+    expect(response.statusCode).toEqual(404);
   });
 });
