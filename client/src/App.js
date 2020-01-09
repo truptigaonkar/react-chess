d18914fd9c898fa0e9570d15b4fc79e655fd9d33
BASEL8 <basel84mn@gmail.com>
2020-01-09 09:38:30 +0100

    add clint and fix eslint

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios'
+const App = () => {
+  return (
+    <div className="App">
+      test
+    </div>
+  );
+}
+
+export default App;
