6c245ec8e33e752f9b5d7c12043f4fea3c1846f9
Ayumi Nakamura <ayumi.nakamur@gmail.com>
2020-01-13 14:49:17 +0100

    fixing button click test

--- client/src/__tests__/Start.test.js
+++ client/src/__tests__/Start.test.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import { create } from 'react-test-renderer';
 import Login from '../components/Start';
+
+const onClick = jest.fn();
+const mockHandleClick = jest.fn();
+const onSearchMock = jest.fn();
 
 afterEach(cleanup);
 
 describe('Start component', () => {
+  it('matches the snapshot', () => {
+    const start = create(<Login />);
+    expect(start.toJSON()).toMatchSnapshot();
+  });
+
   it('renders input field for user Id', () => {
     const { getByTestId } = render(<Login />);
     const input = getByTestId('input');
     expect(input).toBeInTheDocument();
+  });
+
+  it('should call onChange prop with input valud', () => {
+    const component = mount(<Login />)
   });
 
   it('renders Enter button', () => {
@@ -18,6 +32,25 @@ describe('Start component', () => {
     expect(button).toBeInTheDocument();
     expect(button.textContent).toBe('Enter');
   });
+
+  it('captures clicks', () => {
+    const { getByTestId } = render(<Login />);
+    const button = getByTestId('button');
+    fireEvent.click(button);
+
+    
+    /* const handleClick = () => {
+      done();
+    }
+    const { getByTestId } = render(<Login onClick={handleClick} />);
+    const button = getByTestId('button');
+
+    const { getByText } = render(
+      <Button onClick={handleClick}>Enter</Button>
+    );
+    const node = getByText('Enter'); 
+    fireEvent.click(button);*/
+  })
 });
 
 describe('save to localStorage', () => {
