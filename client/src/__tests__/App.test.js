f80df0ff68ada6bd030ea23887bc0e6123797c4a
Ayumi Nakamura <ayumi.nakamur@gmail.com>
2020-01-13 11:17:50 +0100

    App test wip

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { MemoryRouter } from 'react-router'; 
 import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 import App from '../App';
+import Start from '../components/Start';
+import Lobby from '../components/Lobby';
+import Game from '../components/Game';
+
+configure({adapter: new Adapter()});
+render(<App />);
 
 describe('App component', () => {
-  it('renders without crashing', () => {
-    const { getByText } = render(<App />);
-    const container = getByTestId("container");
-    expect(container.children.length).toBe(3);
+  it('renders Start component on root', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[ '/' ]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(wrapper.find(Start)).toHaveLength(1);
+    expect(wrapper.find('Route[path="/lobby"]).first().prop('component')).toBe(Lobby);
+    expect(wrapper.find(Lobby)).toHaveLength(0);
+    expect(wrapper.find(Game)).toHaveLength(0);
   });
+
+  /* it('renders Lobby component', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[ '/lobby' ]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(wrapper.find(Lobby)).toHaveLength(1);
+  });
+
+  it('renders Lobby component', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[ '/game' ]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(wrapper.find(Game)).toHaveLength(1);
+  }); */
 });
